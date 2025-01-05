612ddf324ceb1fc706a5481d0f72a04b81ad78f5
Nayan Mer <nayan_mer@outlook.com>
2025-01-05 15:14:58 +0530

    Commit made from VS Code Azure Static Web Apps

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,12 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: `<div>Hello {{value}}</div>`,
+  template: `<div>Hello {{value}}</div><br/><div>Hello {{Name}} are you ready to build {{WebType}}</div>`,
 })
 export class AppComponent {
   value = 'World';
+  Name = "Rajesh";
+  WebType ="Static Web App and Api";
+
+
 }
